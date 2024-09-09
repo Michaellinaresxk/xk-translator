ce63561624c4f83b7e3ad55a86e9cd9bad1c3c50
Michael Linares <michaellinaresdbildungs@gmail.com>
2024-09-09 13:34:26 +0200

    Added form and emit to parent component.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,19 @@
 <script setup lang="ts">
+import { ref } from "vue"
+import TranslationForm from "../components/TranslatationForm.vue"
+import TranslationResult from "../components/TranslationResult.vue"
+
+
+const translatedText = ref('')
+const translateQuery = (translationQuery: string, selectedLanguage: string) => {
+  console.log(selectedLanguage, translationQuery )
+}
 
 </script>
 
 <template>
   <main>
-     <h1>Clean</h1>
+     <TranslationForm @handleTranslate="translateQuery" />
+     <TranslationResult :translatedText="translatedText" />
   </main>
 </template>
